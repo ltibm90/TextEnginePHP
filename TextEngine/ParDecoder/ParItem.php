200902cc95e70415952334cf4f4b6c75013b39ea
Ibm <ltibm90@gmail.com>
2022-01-24 20:13:09 +0300

    1.5.1 Update
    Fixed array access problem for local variables.

--- TextEngine/ParDecoder/ParItem.php
+++ TextEngine/ParDecoder/ParItem.php
@@ -173,7 +173,7 @@ class ParItem extends InnerItem
                                 else $propertyStr .= '.' . $prevvalue;
 								$allowget = $this->AllowAccessProperty($propertyStr, PropType::Indis);
 								unset($lastPropObject);
-								if($allowget) $lastPropObject = ComputeActions::GetPropEx($prevvalue, $varnew);
+								if($allowget) $lastPropObject = ComputeActions::GetPropEx($prevvalue, $varnew, $localvars);
 								else $lastPropObject = null;
 								unset($prop);
 								if($lastPropObject)	$prop = &$lastPropObject->Value;
